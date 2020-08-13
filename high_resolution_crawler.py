6eaf7702607f0d3b83a58c08d713e9d8d598ecb9
Jaehyeong Park <jwzz123123@gmail.com>
2020-08-13 16:10:09 +0900

    async requests

--- high_resolution_crawler.py
+++ high_resolution_crawler.py
@@ -2,7 +2,7 @@ import time
 import re
 import asyncio
 import requests
-import aiohttp
+import requests_async
 
 master_regex = r'<.*>\s*(?P<정원>\d+)\s*\((?P<재학생>\d+)\)\s*<.*>\s*' \
                r'<.*>\s*(?P<수강신청인원>\d+)\s*<.*>'
@@ -12,9 +12,8 @@ course = {'srchSbjtCd': '033.019', 'workType': 'S'}
 
 
 async def _search_course(course_info_dict: dict):
-    async with aiohttp.ClientSession() as session:
-        async with session.post(url, data=course_info_dict) as search_request:
-            return await search_request.text()
+    async with requests_async.Session() as session:
+        return await session.post(url, course_info_dict)
 
 
 async def _get_regex_info_in_html_text(html_text, regex_expression):
@@ -24,20 +23,28 @@ async def _get_regex_info_in_html_text(html_text, regex_expression):
 
 async def _request_course_info(course_info_dict, regex_expression=master_regex):
     html_text = await _search_course(course_info_dict)
-    find = await _get_regex_info_in_html_text(html_text, regex_expression)
+    find = await _get_regex_info_in_html_text(html_text.text, regex_expression)
     print(find)
     print(time.time())
 
 
+async def _wait(time_out=0.05):
+    await asyncio.sleep(time_out)
+
+
+async def _request_course_info_with_time_limit(course_info_dict, time_out=0.05, regex_expression=master_regex):
+    await asyncio.gather(_wait(time_out), _request_course_info(course_info_dict, regex_expression))
+
+
 async def _main(course_info_dict):
-    await asyncio.gather(*[_request_course_info(course_info_dict) for _ in range(100)])
+    await asyncio.gather(*[_request_course_info_with_time_limit(course_info_dict) for _ in range(5)])
 
 
 def make_info_dict(subject_id, subject_name=None, professor_name=None):
     return {'srchSbjtCd': subject_id, 'workType': 'S'}
 
 
-def request(course_info_dict=None, n=1):
+def request(course_info_dict=None, n=100):
     if course_info_dict is None:
         course_info_dict = course
     for i in range(n):
