e2de267fcf1bf654c968846ab311a76485c9df4f
SyphonArch <jake.hyun@hotmail.com>
2020-08-13 21:16:29 +0900

    Initial sniper.py implementation

--- configurations.py
+++ configurations.py
@@ -1,3 +1,7 @@
+# EMAIL RELATED SETTINGS
 SMTP_HOST = 'smtp.gmail.com'
 SMTP_USER = 'jake.hyun.jh@gmail.com'
 SMTP_SSL_PORT = 465
+
+# SUBJECT RELATED SETTINGS
+target_courses = {'L0440.000600': [3, 13, 23]}
---
+++ sniper.py
@@ -0,0 +1,28 @@
+from time import sleep
+from crawler import course_no_to_records
+import emailer
+from configurations import target_courses
+
+
+def course_identifier(subject_id, course_no):
+    return subject_id, course_no
+
+
+initial_registrant_counts = {}
+
+while True:
+    for subject_id in target_courses:
+        # 모든 강좌번호에 대해 검색해서 결과 저장하기
+        course_nos = target_courses[subject_id]
+        records = course_no_to_records(subject_id, course_nos)
+
+        for record in records:
+            course_id = course_identifier(subject_id, record['강좌번호'])
+            registrant_count = int(record['수강신청인원'])
+            # 첫 검색인 경우, 수강신청인원을 기록한다.
+            if course_id not in initial_registrant_counts:
+                initial_registrant_counts[course_id] = registrant_count
+            else:
+                if registrant_count < initial_registrant_counts[course_id]:
+                    emailer.send(f"{subject_id} 빈자리 알림", f"{subject_id}의 {record['강좌번호']} 분반에 자리가 확인되었습니다.")
+    sleep(3)
