c7be8d29c92c4a975870f4a79fa4fcaa6ea44b53
SyphonArch <jake.hyun@hotmail.com>
2020-08-14 12:34:30 +0900

    Update emailing logic

--- sniper.py
+++ sniper.py
@@ -28,8 +28,8 @@ while True:
                     emailer.send(f"{subject_id} 빈자리 알림",
                                  f"{subject_id}의 {record['강좌번호']} 분반에 자리가 확인되었습니다.\n\n"
                                  f"현재상태:\n\t수강신청인원: {registrant_count} // 정원(재학생): {record['정원(재학생)']}"
-                                 f"sugang.snu.ac.kr")
+                                 f"\n\nsugang.snu.ac.kr")
                     print('Message Sent!')
-                    initial_registrant_counts[course_id] = registrant_count
+                initial_registrant_counts[course_id] = registrant_count
     print(initial_registrant_counts)
     sleep(5)  # delay between checks
