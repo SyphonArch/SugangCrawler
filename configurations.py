eb6e727f9ca5dc15b73f055369ed23d4be3c3155
SyphonArch <jake.hyun@hotmail.com>
2020-08-14 09:39:04 +0900

    Add real target courses

--- configurations.py
+++ configurations.py
@@ -4,4 +4,4 @@ SMTP_USER = 'jake.hyun.jh@gmail.com'
 SMTP_SSL_PORT = 465
 
 # SUBJECT RELATED SETTINGS
-target_courses = {'L0440.000600': [3, 13, 23]}
+target_courses = {'033.019': [4, 6, 7], '033.020': [5, 6, 12]}  # 통계, 통실
